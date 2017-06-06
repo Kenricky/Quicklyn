e34d7cd2b9823d56c09f8175224d5122ae26d217
gpadilha <guilhermepmaia@gmail.com>
2017-06-06 12:36:21 -0700

    first step of post creation done.

--- app/src/main/java/com/quicklyn/quicklyn/PostStep1Activity.java
+++ app/src/main/java/com/quicklyn/quicklyn/PostStep1Activity.java
@@ -1,13 +1,67 @@
 package com.quicklyn.quicklyn;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PostStep1Activity extends AppCompatActivity {
+
+    private Button mButtonContinue;
+    private EditText mPostTitle;
+    private EditText mPostDescription;
+    private EditText mPostPrice;
+
+
+    private static final String TITLE = "title_data";
+    private static final String DESCRIPTION = "description_data";
+    private static final String PRICE = "price_data";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_step1);
+
+        mButtonContinue = (Button) findViewById(R.id.post1_button_continue);
+        mPostTitle = (EditText) findViewById(R.id.title_text);
+        mPostDescription = (EditText) findViewById(R.id.description_text);
+        mPostPrice = (EditText) findViewById(R.id.price_text);
+
+        mButtonContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(validateForm()) {
+                    Intent intent = new Intent(PostStep1Activity.this, PostStep2Activity.class);
+                    intent.putExtra(TITLE, mPostTitle.getText());
+                    intent.putExtra(DESCRIPTION, mPostDescription.getText());
+                    intent.putExtra(PRICE, mPostPrice.getText());
+                    startActivity(intent);
+                }
+            }
+        });
     }
+
+    private boolean validateForm() {
+        if(mPostTitle.getText().toString().equals("") || mPostTitle.getText().length() < 5){
+            Toast.makeText(PostStep1Activity.this, R.string.invalid_title, Toast.LENGTH_SHORT).show();
+            mPostTitle.requestFocus();
+            return false;
+        }
+        if(mPostDescription.getText().toString().trim().equals("") || mPostDescription.getText().length() < 5){
+            Toast.makeText(PostStep1Activity.this, R.string.invalid_description, Toast.LENGTH_SHORT).show();
+            mPostDescription.requestFocus();
+            return false;
+        }
+        if(mPostPrice.getText().toString().equals("") || new Double(mPostPrice.getText().toString()) <= 0){
+            Toast.makeText(PostStep1Activity.this, R.string.invalid_price, Toast.LENGTH_SHORT).show();
+            mPostPrice.requestFocus();
+            return false;
+        }
+        return true;
+    }
+
 }
--- app/src/main/java/com/quicklyn/quicklyn/PostStep2Activity.java
+++ app/src/main/java/com/quicklyn/quicklyn/PostStep2Activity.java
@@ -1,13 +1,55 @@
 package com.quicklyn.quicklyn;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
-public class PostStep2Activity extends AppCompatActivity {
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+public class PostStep2Activity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
+
+    private EditText mLocation;
+    private RadioGroup mTypeGroup;
+    private EditText mOtherText;
+    private EditText mPostDate;
+
+    private DatePickerDialog mDatePickerDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_step2);
+
+        mLocation = (EditText) findViewById(R.id.post_location);
+        mTypeGroup = (RadioGroup) findViewById(R.id.type_group);
+        mOtherText = (EditText) findViewById(R.id.other_text);
+        mPostDate = (EditText) findViewById(R.id.post_date);
+
+
+        GregorianCalendar calendar = new GregorianCalendar();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        mDatePickerDialog = new DatePickerDialog(
+                PostStep2Activity.this, PostStep2Activity.this, calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+
+        mPostDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDatePickerDialog.show();
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
     }
 }
