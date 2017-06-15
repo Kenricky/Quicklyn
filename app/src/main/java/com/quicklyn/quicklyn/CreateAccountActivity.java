4b9ed89c1bb0e4f849888cadd0b1945e809215c3
kenneth ukwunna <kennethukwunna@gmail.com>
2017-06-15 11:14:05 -0700

    Login and create account

---
+++ app/src/main/java/com/quicklyn/quicklyn/CreateAccountActivity.java
@@ -0,0 +1,88 @@
+package com.quicklyn.quicklyn;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class CreateAccountActivity extends LoginActivity {
+
+    private EditText mName;
+    private EditText mEmail;
+    private EditText mPassword;
+    private EditText mConfirmPassword;
+    private Button mButtonSignIn;
+    private DatabaseReference mDatabase;
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        requestWindowFeature(R.string.create_account);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mName = (EditText) findViewById(R.id.edit_name);
+        mEmail = (EditText) findViewById(R.id.edit_email);
+        mPassword = (EditText) findViewById(R.id.edit_password);
+        mConfirmPassword = (EditText) findViewById(R.id.edit_confirm_password);
+        mButtonSignIn = (Button) findViewById(R.id.button_crete_account);
+
+
+        mButtonSignIn.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                if(validateForm()){
+                    User user = new User();
+                    user.setName(mName.getText().toString());
+                    user.setEmail(mEmail.getText().toString());
+                    user.setPassword(mPassword.getText().toString());
+
+                    mDatabase.child("users").push().setValue(user);
+                    Toast.makeText(CreateAccountActivity.this, R.string.account_created, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private boolean validateForm() {
+        if (mName.getText().toString().equals("")) {
+            Toast.makeText(CreateAccountActivity.this, R.string.error_name_required, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (mEmail.getText().toString().equals("") || !mEmail.getText().toString().contains("@")) {
+            Toast.makeText(CreateAccountActivity.this, R.string.error_email_required, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (mPassword.getText().toString().equals("")) {
+            Toast.makeText(CreateAccountActivity.this, R.string.error_password_required, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        for(int i=0; i<mPassword.getText().toString().length(); i++){
+            if(isNumeric(mPassword.getText().toString().charAt(i))){
+                break;
+            }
+            Toast.makeText(CreateAccountActivity.this, R.string.error_password_has_no_number, Toast.LENGTH_LONG).show();
+        }
+        if (!mPassword.getText().toString().equals(mConfirmPassword.getText().toString())) {
+            Toast.makeText(CreateAccountActivity.this, R.string.error_password_match, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isNumeric(char x){
+        try{
+            new Double(x);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+}
--- app/src/main/java/com/quicklyn/quicklyn/LoginActivity.java
+++ app/src/main/java/com/quicklyn/quicklyn/LoginActivity.java
@@ -1,8 +1,63 @@
 package com.quicklyn.quicklyn;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity  {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LoginActivity extends Activity {
+
+    private EditText mUsername;
+    private EditText mPassword;
+    private Button mButtonSignIn;
+    private TextView mLoginMessage;
+
+    private DatabaseReference mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Initialization
+        mUsername = (EditText) findViewById(R.id.edit_username);
+        mPassword = (EditText) findViewById(R.id.edit_password);
+        mButtonSignIn = (Button) findViewById(R.id.button_login);
+        mLoginMessage = (TextView) findViewById(R.id.login_message);
+
+        mButtonSignIn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                mDatabase = FirebaseDatabase.getInstance().getReference();
+
+                // Validates the User name and Password for admin, admin
+                if (mUsername.getText().toString().equals("admin") && mPassword.getText().toString().equals("admin")) {
+                    mLoginMessage.setText(R.string.login_successful);
+                } else {
+                    mLoginMessage.setText(R.string.error_login_fail);
+                }
+            }
+        });
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
 
